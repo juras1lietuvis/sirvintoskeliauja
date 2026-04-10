1272fc1a2fa7ce35f6c5ea6c3b109007f1364888
juras1lietuvis <217199300+juras1lietuvis@users.noreply.github.com>
2026-04-10 21:11:34 +0300

    Google Maps

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Širvintos keliauja — konfigūracija
 // Įrašyk savo Google Maps API raktą žemiau:
 window.BRIEDZIU_CONFIG = {
-  GOOGLE_MAPS_API_KEY: 'PASTE_YOUR_GOOGLE_MAPS_API_KEY_HERE'
+  GOOGLE_MAPS_API_KEY: 'AIzaSyBG5948KdQIsIi3kdZm44z5enQdrwCMWWc'
 };
